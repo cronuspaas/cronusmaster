c08458199e68006e106ef00b51dac39c42ac01af
Bin Yu <yubin154@gmail.com>
2014-07-22 00:38:18 -0700

    move user data dir outside deploy package

--- AgentMaster/app/jobs/PlayVarUtils.java
+++ AgentMaster/app/jobs/PlayVarUtils.java
@@ -48,7 +48,7 @@ public class PlayVarUtils extends VarUtils {
 		userDataDaoType = getVarStr("agentmaster.userDataDao", "file");
 		
 		// file
-		appHome = getVarStr("agentmaster.userDataDao.file.root", ".");
+		appHome = getVarStr("agentmaster.userDataDao.file.root", null);
 		userDataDir = getVarStr("agentmaster.userDataDao.file.dir", "user_data"); 
 		
 		// s3
