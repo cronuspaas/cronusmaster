5895a801e4dfc6e680d608f1cf56914c646b307a
bin yu <yubin154@gmail.com>
2014-07-24 01:20:14 -0700

    lcm

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf target
-curl -sS 'https://raw.githubusercontent.com/yubin154/cronuspackages/master/common_scripts/package.sh' | DIR=. bash
+curl -sS 'https://raw.githubusercontent.com/yubin154/cronuspackages/master/common_scripts/package.sh' | DIR=. appName=cronusmaster version="0.1.`date +%Y%m%d%H%M`" bash
 mkdir target
 mv *.cronus *.cronus.prop target/
 
