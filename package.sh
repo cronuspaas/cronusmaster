a126bdf3c2e04e41d2cf8fc34ae61d0bd9e39f95
bin yu <yubin154@gmail.com>
2014-08-22 23:42:45 +0000

    package.sh

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 rm -rf target
 
-curl -sSL 'https://github.com/yubin154/cronusagent/raw/master/agent/scripts/package/package.sh' | DIR=. appname=cronusmaster version="0.1.`date +%Y%m%d%H%M`" pkgsrc="AgentMaster cronus play-1.2.4" bash
+curl -sSL 'http://www.stackscaling.com/downloads/package_cronus' | DIR=. appname=cronusmaster version="0.1.`date +%Y%m%d%H%M`" pkgsrc="AgentMaster cronus play-1.2.4" bash
 
 mkdir target
 mv *.cronus *.cronus.prop target/
